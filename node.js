9b5a05184b0d358c877eae7bad3fbfb43a74b124
dlwogh1227 <81740259+dlwogh1227@users.noreply.github.com>
2021-05-04 21:34:29 +0900

    upbit market price

--- node.js
+++ node.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
+const fs = require('fs');
 const fetch = require('node-fetch')
-let url = 'https://api.upbit.com/v1/market/all?isDetails=false'   //업비트 api
-const options = {method: 'GET'}
 
 app.get('/', (req, res) => {
-  
+    fs.readFile('./BTCname.json', 'utf8', (error, data) => {        
+        if(error) return console.log(error);
+        const BTCname = JSON.parse(data);
+        let url = `https://api.upbit.com/v1/ticker?markets=${BTCname[0].market}`;
+        const options = {method: 'GET'};
+        fetch(url, options)                            //업비트 가격정보 불러오기
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            res.send();
+        })        
+        .catch(err => console.error('error:' + err));
+    })
 })
 
-app.listen(port, () => {
-  
-})
+app.listen(3000, () => {})
